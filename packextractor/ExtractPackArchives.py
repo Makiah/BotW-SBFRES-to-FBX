e55e2b4bf35aa2ae3eb62e731634b9de1ab74dff
Makiah (Windows Virtual Computer) <makiahtbennett@gmail.com>
2017-06-28 11:43:17 -0400

    Begin to work on shift to OBJ extraction rather than FBX (nowhere near complete :P)

--- packextractor/ExtractPackArchives.py
+++ packextractor/ExtractPackArchives.py
@@ -2,9 +2,8 @@ import os
 from customUtilities import CommandLineUtils
 
 def extractAllPackArchives():
-
     # SARC Extract library.
-    sarcExtractPYPath = os.path.join("Libraries", "WiiUTools-master", "SARCTools", "SARCExtract.py")
+    sarcExtractPYPath = os.path.join(os.getcwd(), "Libraries", "WiiUTools-master", "SARCTools", "SARCExtract.py")
 
     # Query the user on both where the existent compilation is, and where to put these files.
     botwContent = "Z:\Desktop\BOTW\Raw Assets" # My personal location.
@@ -22,7 +21,7 @@ def extractAllPackArchives():
                 if not os.path.exists(expectedDirectory):
                     # Extract every PACK archive (has to use Python 2.7), arguments already quoted by custom lbrary.
                     print("Extracting " + file + " to " + expectedDirectory)
-                    CommandLineUtils.call("python", [sarcExtractPYPath, os.path.join(root, file)])
+                    CommandLineUtils.call("python", [CommandLineUtils.toQuotedPath(sarcExtractPYPath), CommandLineUtils.toQuotedPath(os.path.join(root, file))])
                 else:
                     print("Skipping " + file + ", expected directory already exists at " + expectedDirectory)
 
